bdf3467df8c8e03174181f205313e05ebe619e89
William Kirk <whk@whk.io>
2020-05-15 17:24:35 -0500

    initial framework skeletons

---
+++ machine/src/machine.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Machine`
+module Machine
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
---
+++ monitor/src/monitor.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Monitor`
+module Monitor
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
